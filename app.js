7fa19dc162fc5f9503138569e63a414290fbdf96
Hongseongyun <syun4224@naver.com>
2025-05-14 13:27:57 +0900

    05141327

--- app.js
+++ app.js
@@ -12,14 +12,23 @@ async function getRequest(url) {
 async function getProducts() {
   const getProductsUrl =
     'https://www.dabipyeung.com/baexang_back/product/get_products?cate=dp&limit=8&sort=new';
+  const filterWrapper = document.querySelector('.filter-images');
   // trycatch
   try {
     const imageData = await getRequest(getProductsUrl);
-    console.log(imageData);
-    console.log(imageData[0].pr_img, imageData[0].pr_type);
-    //구조분해 할당
-    const { pr_img, pr_type } = imageData[0];
-    console.log(pr_img);
+
+    imageData.map((item) => {
+      //구조분해 할당
+      const { pr_img, pr_type } = item;
+      const itemElement = `
+      <div class="filter-image" data-filter="${pr_type}">
+        <span><img src="${pr_img}" alt="" /></span>
+      </div>
+      `;
+      filterWrapper.insertAdjacentHTML('beforeend', itemElement);
+    });
+    const filterImages = document.querySelectorAll('.filter-image');
+    getItemElements(filterImages);
   } catch (error) {
     console.log(`Error: ${error}`);
   }
@@ -29,8 +38,12 @@ getProducts();
 
 // 버튼 생성 및 버튼 클릭 기능
 
-function getItemElements() {
-  const btnChars = ['all', 'bag', 'shoes', 'watch', 'camera', 'headphone'];
+function getItemElements(imgItems) {
+  imgItems.forEach((item) => {
+    item.addEventListener('click', showLightBox);
+  });
+
+  const btnChars = ['all', 'bag', 'shoe', 'watch', 'camera', 'headphone'];
   const btnsWrapper = document.querySelector('.filter-btns');
 
   btnChars.map((btnChar) => {
@@ -48,6 +61,21 @@ function getItemElements() {
       btn.classList.remove('active');
     });
     this.classList.add('active');
+
+    const selectBtn = this.getAttribute('data-filter');
+    imgItems.forEach((imgItem) => {
+      imgItem.classList.add('hide');
+      const selectedImageType = imgItem.getAttribute('data-filter');
+      setTimeout(() => {
+        if (selectedImageType === selectBtn || selectBtn === 'all') {
+          imgItem.classList.remove('hide');
+          imgItem.classList.add('show');
+        } else {
+          imgItem.classList.remove('show');
+          imgItem.classList.add('hide');
+        }
+      }, 100);
+    });
   }
 
   // button을 클릭 했을 때 activateFilter 함수 실행
@@ -56,4 +84,34 @@ function getItemElements() {
   });
 }
 
-getItemElements();
+const lightBox = document.querySelector('.light-box');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close');
+
+// Light Box 활성화 기능
+function showLightBox() {
+  const selectedImage = this.querySelector('img').src;
+  // getAttribute
+  // <a href="http://abc/com"></a>
+  // getAttribute("href") : http://abc/com
+  //getAttribute("href", "http://abc/com") : <a href="http://abc/com"></a>
+  const categoryName = this.getAttribute('data-filter');
+
+  const lightBoxImage = document.querySelector('.light-box-image img');
+  const lightBoxCategoty = document.querySelector('.title p');
+
+  lightBoxImage.setAttribute('src', selectedImage);
+  lightBoxCategoty.textContent = categoryName;
+
+  lightBox.style.display = 'block';
+  overlay.style.display = 'block';
+}
+
+function closeLightBox() {
+  lightBox.style.display = 'none';
+  overlay.style.display = 'none';
+}
+
+[overlay, closeBtn].forEach((elmt) => {
+  elmt.addEventListener('click', closeLightBox);
+});
